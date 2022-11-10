f0ab123ee0bb36b2443859cf3fefbe4c2f68bd50
Maximov <m@m.com>
2022-11-10 15:55:12 -0500

    final 1.12

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -26,236 +26,138 @@ class DifferTest {
         String file1 = "/file1.json";
         String file2 = "/file2.json";
         String formatName = "stylish";
-        String fileExpected = "/exp1.json";
+        String fileExpected = "/stylish1.txt";
         String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
         assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferStylish2() throws IOException {
-        String file1 = "file5.json";
-        String file2 = "file0.json";
+        String file1 = "/file5.json";
+        String file2 = "/file0.json";
         String formatName = "stylish";
-        String expected = """
-                {
-                  - host: hexlet.io
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/stylish2.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferStylish3() throws IOException {
-        String file1 = "file0.json";
-        String file2 = "file6.json";
+        String file1 = "/file0.json";
+        String file2 = "/file6.json";
         String formatName = "stylish";
-        String expected = """
-                {
-                  + timeout: 20
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/stylish3.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferStylish4() throws IOException {
-        String file1 = "file0.json";
-        String file2 = "file0.json";
+        String file1 = "/file0.json";
+        String file2 = "/file0.json";
         String formatName = "stylish";
-        String expected = """
-                {
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/stylish0.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferStylish5() throws IOException {
-        String file1 = "file3.json";
-        String file2 = "file4.json";
+        String file1 = "/file3.json";
+        String file2 = "/file4.json";
         String formatName = "stylish";
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/stylish4.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferStylishYaml1() throws IOException {
-        String file1 = "file7.yml";
-        String file2 = "file8.yml";
+        String file1 = "/file7.yml";
+        String file2 = "/file8.yml";
         String formatName = "stylish";
-        String expected = """
-                {
-                    chars1: [a, b, c]
-                  - chars2: [d, e, f]
-                  + chars2: false
-                  - checked: false
-                  + checked: true
-                  - default: null
-                  + default: [value1, value2]
-                  - id: 45
-                  + id: null
-                  - key1: value1
-                  + key2: value2
-                    numbers1: [1, 2, 3, 4]
-                  - numbers2: [2, 3, 4, 5]
-                  + numbers2: [22, 33, 44, 55]
-                  - numbers3: [3, 4, 5]
-                  + numbers4: [4, 5, 6]
-                  - setting1: Some value
-                  + setting1: Another value
-                  - setting2: 200
-                  + setting2: 300
-                  - setting3: true
-                  + setting3: none
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/stylish5.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferPlain1() throws IOException {
-        String file1 = "file3.json";
-        String file2 = "file4.json";
+        String file1 = "/file3.json";
+        String file2 = "/file4.json";
         String formatName = "plain";
-        String expected = """
-                Property 'chars2' was updated. From [complex value] to false
-                Property 'checked' was updated. From false to true
-                Property 'default' was updated. From null to [complex value]
-                Property 'id' was updated. From 45 to null
-                Property 'key1' was removed
-                Property 'key2' was added with value: 'value2'
-                Property 'numbers2' was updated. From [complex value] to [complex value]
-                Property 'numbers3' was removed
-                Property 'numbers4' was added with value: [complex value]
-                Property 'setting1' was updated. From 'Some value' to 'Another value'
-                Property 'setting2' was updated. From 200 to 300
-                Property 'setting3' was updated. From true to 'none'""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/plain1.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferPlain2() throws IOException {
-        String file1 = "file5.json";
-        String file2 = "file0.json";
+        String file1 = "/file5.json";
+        String file2 = "/file0.json";
         String formatName = "plain";
-        String expected = "Property 'host' was removed";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/plain2.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferPlain3() throws IOException {
-        String file1 = "file0.json";
-        String file2 = "file6.json";
+        String file1 = "/file0.json";
+        String file2 = "/file6.json";
         String formatName = "plain";
-        String expected = "Property 'timeout' was added with value: 20";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/plain3.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferPlain4() throws IOException {
-        String file1 = "file0.json";
-        String file2 = "file0.json";
+        String file1 = "/file0.json";
+        String file2 = "/file0.json";
         String formatName = "plain";
-        String expected = "";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/plain0.txt";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferJson1() throws IOException {
-        String file1 = "file3.json";
-        String file2 = "file4.json";
+        String file1 = "/file3.json";
+        String file2 = "/file4.json";
         String formatName = "json";
-        String expected = """
-                {
-                  "chars1" : [ "a", "b", "c" ],
-                  "chars2" : false,
-                  "checked" : true,
-                  "default" : [ "value1", "value2" ],
-                  "id" : null,
-                  "key2" : "value2",
-                  "numbers1" : [ 1, 2, 3, 4 ],
-                  "numbers2" : [ 22, 33, 44, 55 ],
-                  "numbers4" : [ 4, 5, 6 ],
-                  "setting1" : "Another value",
-                  "setting2" : 300,
-                  "setting3" : "none"
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/expect1.json";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferJson2() throws IOException {
-        String file1 = "file5.json";
-        String file2 = "file0.json";
+        String file1 = "/file5.json";
+        String file2 = "/file0.json";
         String formatName = "json";
-        String expected = "{ }";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/expect0.json";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferJson3() throws IOException {
-        String file1 = "file0.json";
-        String file2 = "file6.json";
+        String file1 = "/file0.json";
+        String file2 = "/file6.json";
         String formatName = "json";
-        String expected = """
-                {
-                  "timeout" : 20
-                }""";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/expect2.json";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 
     @Test
     void testDifferJson4() throws IOException {
-        String file1 = "file0.json";
-        String file2 = "file0.json";
+        String file1 = "/file0.json";
+        String file2 = "/file0.json";
         String formatName = "json";
-        String expected = "{ }";
-        String actual = Differ.generate(absolutePath + "/" + file1, absolutePath
-                + "/" + file2, formatName);
-        assertEquals(expected, actual);
+        String fileExpected = "/expect0.json";
+        String actual = Differ.generate(absolutePath + file1, absolutePath + file2, formatName);
+        assertEquals(Files.readString(Path.of(absolutePath + fileExpected)), actual);
     }
 }
