95b460fe5f546d4b82a778d324ff5b7af15e8dbc
Maximov <m@m.com>
2022-11-12 12:24:58 -0500

    final 1.17

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,7 +2,6 @@ package hexlet.code;
 
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
